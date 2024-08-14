eda0beab1c609241a2b4163b98faacbb0426d08d
Erkan Yetişgin <122626424+erkanyetisgin@users.noreply.github.com>
2024-08-14 15:04:06 +0300

    fixed index.ts

---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+import { BASE_URL, SUPABASE_ANON_KEY, SUPABASE_URL } from '@env';
+
+export const supabaseUrl = SUPABASE_URL;
+export const supabaseAnonKey = SUPABASE_ANON_KEY;
+export const baseUrl = BASE_URL;
